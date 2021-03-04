3cb5541bfd46bbed02486196dcb983d0e75a9532
Yerkhan <baigal.erhan@gmail.com>
2021-03-05 03:33:08 +0600

    added bomberman animation

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-class Player {
-	constructor(left = 0, top = 0, size = 30, step = 5) {
+class Bomberman {
+	constructor(left = 0, top = 0, size = 32, step = 2) {
 		this.left = left
 		this.top = top
 		this.size = size
@@ -7,11 +7,6 @@ class Player {
 		this.keysPressed = {}
 		this.init()
 	}
-
-	getLeft = () => this.left
-	getRight = () => this.left + this.size
-	getTop = () => this.top
-	getBottom = () => this.top + this.size
 
 	setLeft = val => {
 		this.left = parseFloat(val)
@@ -39,13 +34,12 @@ class Player {
 
 	createHTML = () => {
 		this.root = document.querySelector('#root')
-		this.div = document.createElement('div')
-		this.div.classList.add('player')
+		this.div = document.querySelector('#bomberman')
+		this.img = document.querySelector('#bomberman-sprite')
 		this.div.style.left = `${this.left}px`
 		this.div.style.top = `${this.top}px`
 		this.div.style.width = `${this.size}px`
 		this.div.style.height = `${this.size}px`
-		this.root.append(this.div)
 	}
 
 	addEventListeners = () => {
@@ -57,35 +51,58 @@ class Player {
 		})
 	}
 
+	removeClasses = () => {
+		this.img.className = ''
+	}
+
+	addLookDirection = direction => {
+		this.img.className = `bomberman-look-${direction}`
+	}
+
+	moveLeft = (root, div) => {
+		this.updateLeft(-Math.min(Math.abs(root - div + 1), this.step))
+		this.removeClasses()
+		this.img.classList.add('bomberman-walk-left')
+		return 'left'
+	}
+	moveRight = (root, div) => {
+		this.updateLeft(Math.min(Math.abs(root - div - 1), this.step))
+		this.removeClasses()
+		this.img.classList.add('bomberman-walk-right')
+		return 'right'
+	}
+	moveUp = (root, div) => {
+		this.updateTop(-Math.min(Math.abs(root - div + 1), this.step))
+		this.removeClasses()
+		this.img.classList.add('bomberman-walk-up')
+		return 'up'
+	}
+	moveDown = (root, div) => {
+		this.updateTop(Math.min(Math.abs(root - div - 1), this.step))
+		this.removeClasses()
+		this.img.classList.add('bomberman-walk-down')
+		return 'down'
+	}
+
 	animate = () => {
+		let direction
 		const callback = () => {
 			const root = this.root.getBoundingClientRect()
 			const div = this.div.getBoundingClientRect()
-			if (this.keysPressed['KeyA'] && div.left > root.left + this.step)
-				this.moveLeft()
-			if (this.keysPressed['KeyD'] && div.right < root.right - this.step)
-				this.moveRight()
-			if (this.keysPressed['KeyW'] && div.top > root.top + this.step)
-				this.moveUp()
-			if (this.keysPressed['KeyS'] && div.bottom < root.bottom - this.step)
-				this.moveDown()
+			if (direction)
+				this.addLookDirection(direction)
+			if (this.keysPressed['KeyA'] && div.left > root.left)
+				direction = this.moveLeft(root.left, div.left)
+			if (this.keysPressed['KeyD'] && div.right < root.right)
+				direction = this.moveRight(root.right, div.right)
+			if (this.keysPressed['KeyW'] && div.top > root.top)
+				direction = this.moveUp(root.top, div.top)
+			if (this.keysPressed['KeyS'] && div.bottom < root.bottom)
+				direction = this.moveDown(root.bottom, div.bottom)
 			requestAnimationFrame(callback)
 		}
 		requestAnimationFrame(callback)
 	}
-
-	moveLeft = () => {
-		this.updateLeft(-this.step)
-	}
-	moveRight = () => {
-		this.updateLeft(this.step)
-	}
-	moveUp = () => {
-		this.updateTop(-this.step)
-	}
-	moveDown = () => {
-		this.updateTop(this.step)
-	}
 }
 
-const player = new Player()
+const player = new Bomberman()
