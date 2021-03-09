3ef4eedccedd190ef2e957f25ab2fa10f4e9348b
Yerkhan <baigal.erhan@gmail.com>
2021-03-10 03:41:26 +0600

    update: added enemies, random direction

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ const getRandomInt = (min, max) => {
 	return Math.floor(Math.random() * (max - min)) + min
 }
 
+const getRandomDirection = (directions = ['left', 'right', 'up', 'down']) => {
+	return directions[getRandomInt(0, directions.length)]
+}
+
 class Block {
 	constructor({x = 1, y = 1, board} = {}) {
 		this.x = x
@@ -61,12 +65,12 @@ class Wall extends Block {
 }
 
 class Entity {
-	constructor({board, pixelSize = 1} = {}) {
+	constructor({board, pixelSize = 1, left, top, speed = 1} = {}) {
 		this.board = board
 		this.pixelSize = pixelSize
-		this.speed = 1
-		this.left = pixelSize * 2
-		this.top = pixelSize * 2
+		this.speed = speed || Math.floor(pixelSize / 2)
+		this.left = left || pixelSize * 2
+		this.top = top || pixelSize * 2
 		this.size = 16 * pixelSize * 0.75
 
 		this.initialize()
@@ -76,6 +80,7 @@ class Entity {
 	createHTML = () => {
 		this.div = document.createElement('div')
 		this.div.classList.add('pixel-art')
+		this.div.style.position = 'absolute'
 		this.img = document.createElement('img')
 		this.div.append(this.img)
 		this.board.append(this.div)
@@ -104,6 +109,53 @@ class Entity {
 		this.div.style.height = `${this.size}px`
 		this.div.style.width = `${this.size}px`
 
+	}
+
+	initialize = () => {
+		this.createHTML()
+	}
+}
+
+class Enemy extends Entity {
+	constructor({board, pixelSize = 1, left, top} = {}) {
+		super({board, pixelSize, left, top})
+		this.speed /= 2
+		this.direction = getRandomDirection()
+
+		this.initialize()
+	}
+
+	createHTML = () => {
+		this.div.className = 'enemy pixel-art'
+		this.img.src = './img/enemy.png'
+	}
+
+	moveLeft() {
+		super.moveLeft()
+		this.img.className = 'pixel-art enemy-walk-left'
+		this.direction = 'left'
+	}
+
+	moveRight() {
+		super.moveRight()
+		this.img.className = 'pixel-art enemy-walk-right'
+		this.direction = 'right'
+	}
+
+	moveUp() {
+		super.moveUp()
+		this.img.className = 'pixel-art enemy-walk-up'
+		this.direction = 'up'
+	}
+
+	moveDown() {
+		super.moveDown()
+		this.img.className = 'pixel-art enemy-walk-down'
+		this.direction = 'down'
+	}
+
+	die() {
+		this.img.className = 'pixel-art enemy-die'
 	}
 
 	initialize = () => {
@@ -148,21 +200,28 @@ class Bomberman extends Entity {
 		this.direction = 'down'
 	}
 
+	die() {
+		this.img.className = 'pixel-art bomberman-die'
+	}
+
 	initialize = () => {
 		this.createHTML()
 	}
 }
 
 class Game {
-	constructor({rows = 13, columns = 31, pixelSize = 1} = {}) {
+	constructor({rows = 13, columns = 31, pixelSize = 1, enemyCount = 5} = {}) {
 		this.rows = rows
 		this.columns = columns
 		this.pixelSize = pixelSize
 		this.keysPressed = {}
 		this.board = document.querySelector('#board')
+		this.size = 16 * this.pixelSize
+		this.enemyCount = enemyCount
 		this.rocks = []
 		this.walls = []
 		this.bomberman = new Bomberman({board: this.board, pixelSize: this.pixelSize})
+		this.enemies = []
 
 		this.initialize()
 	}
@@ -202,8 +261,22 @@ class Game {
 		while (sum < wallsCount) {
 			const x = getRandomInt(1, this.columns),
 				y = getRandomInt(1, this.rows)
-			if (!this.isBlock(x, y) && !(x <= 4 && y <= 4)) {
+			if (!this.isBlock(x, y) && !(x <= 3 && y <= 3)) {
 				this.walls.push(new Wall({x, y, board: this.board}))
+				sum++
+			}
+		}
+	}
+
+	createEnemies = () => {
+		let sum = 0
+		while (sum < this.enemyCount) {
+			const x = getRandomInt(1, this.columns),
+				y = getRandomInt(1, this.rows)
+			if (!this.isBlock(x, y) && !(x <= 5 && y <= 5)) {
+				const left = this.size * (x - 2) + (2 * this.pixelSize),
+					top = this.size * (y - 2) + (2 * this.pixelSize)
+				this.enemies.push(new Enemy({left, top, board: this.board, pixelSize: this.pixelSize}))
 				sum++
 			}
 		}
@@ -212,22 +285,23 @@ class Game {
 	createHTML = () => {
 		this.createRocks()
 		this.createWalls()
+		this.createEnemies()
 	}
 
 	createCSS = () => {
 		const style = document.createElement('style')
 		style.innerHTML = `
 			#board {
-				grid-template-rows: repeat(${this.rows}, ${16 * this.pixelSize}px);
-				grid-template-columns: repeat(${this.columns}, ${16 * this.pixelSize}px);
+				grid-template-rows: repeat(${this.rows}, ${this.size}px);
+				grid-template-columns: repeat(${this.columns}, ${this.size}px);
 			}`
 		document.querySelector('head').append(style)
 		this.updateSizes()
 	}
 
 	updateSizes = () => {
-		this.board.style.width = `${16 * this.columns * this.pixelSize}px`
-		this.board.style.height = `${16 * this.rows * this.pixelSize}px`
+		this.board.style.width = `${this.size * this.columns}px`
+		this.board.style.height = `${this.size * this.rows}px`
 	}
 
 	addEventListeners = () => {
@@ -239,15 +313,11 @@ class Game {
 		})
 	}
 
-	draw = () => {
-		this.bomberman.draw()
-	}
-
-	update = () => {
-		const left = (this.bomberman.left - 1) / (this.pixelSize * 16) + 2,
-			right = (this.bomberman.left + this.bomberman.size) / (this.pixelSize * 16) + 2,
-			top = (this.bomberman.top - 1) / (this.pixelSize * 16) + 2,
-			bottom = (this.bomberman.top + this.bomberman.size) / (this.pixelSize * 16) + 2
+	updateBomberman = () => {
+		const left = (this.bomberman.left - 1) / this.size + 2,
+			right = (this.bomberman.left + this.bomberman.size) / this.size + 2,
+			top = (this.bomberman.top - 1) / this.size + 2,
+			bottom = (this.bomberman.top + this.bomberman.size) / this.size + 2
 		let moved = false
 		if (this.keysPressed['KeyA'] && !this.keysPressed['KeyD']) {
 			if (!this.isBlock(left, top + 0.05) && !this.isBlock(left, bottom - 0.05)) {
@@ -274,6 +344,58 @@ class Game {
 			}
 		}
 		if (!moved) this.bomberman.img.className = `pixel-art bomberman-look-${this.bomberman.direction}`
+	}
+
+	updateEnemy = enemy => {
+		const left = (enemy.left - 1) / this.size + 2,
+			right = (enemy.left + enemy.size) / this.size + 2,
+			top = (enemy.top - 1) / this.size + 2,
+			bottom = (enemy.top + enemy.size) / this.size + 2
+		if (enemy.direction === 'left') {
+			if (!this.isBlock(left, top + 0.05) && !this.isBlock(left, bottom - 0.05)) {
+				enemy.moveLeft()
+			} else {
+				enemy.direction = getRandomDirection(['right', 'up', 'down'])
+			}
+			return
+		}
+		if (enemy.direction === 'right') {
+			if (!this.isBlock(right, top + 0.05) && !this.isBlock(right, bottom - 0.05)) {
+				enemy.moveRight()
+			} else {
+				enemy.direction = getRandomDirection(['left', 'up', 'down'])
+			}
+			return
+		}
+		if (enemy.direction === 'up') {
+			if (!this.isBlock(left + 0.05, top) && !this.isBlock(right - 0.05, top)) {
+				enemy.moveUp()
+			} else {
+				enemy.direction = getRandomDirection(['left', 'right', 'down'])
+			}
+			return
+		}
+		if (enemy.direction === 'down') {
+			if (!this.isBlock(left + 0.05, bottom) && !this.isBlock(right - 0.05, bottom)) {
+				enemy.moveDown()
+			} else {
+				enemy.direction = getRandomDirection(['left', 'right', 'up'])
+			}
+		}
+	}
+
+	update = () => {
+		this.updateBomberman()
+		this.enemies.forEach(enemy => {
+			this.updateEnemy(enemy)
+		})
+	}
+
+	draw = () => {
+		this.bomberman.draw()
+		this.enemies.forEach(enemy => {
+			enemy.draw()
+		})
 	}
 
 	animate = () => {
