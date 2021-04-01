ff5a7f48ccbd8c2a4ed226ac39d1ef9b8baa0270
Yerkhan <baigal.erhan@gmail.com>
2021-04-01 22:21:19 +0600

    update: added mainMenu class

---
+++ js/game/mainMenu/mainMenu.js
@@ -0,0 +1,35 @@
+import {MAIN_MENU} from '../../utils/constants.js'
+
+export class MainMenu {
+	constructor() {
+		this.div = document.querySelector('#main-menu')
+		this.items = document.querySelectorAll('.main-menu-item')
+		this.selected = MAIN_MENU.START
+
+		this.addEventListener()
+	}
+
+	draw = () => {
+		this.items.forEach((item, i) => {
+			item.className = 'main-menu-item'
+			if (this.selected === i)
+				item.classList.add('main-menu-item-selected')
+		})
+	}
+
+	addEventListener = () => {
+		document.addEventListener('keydown', e => {
+			if (e.code === 'ArrowDown')
+				this.selected = (this.selected + 1) % this.items.length
+			else if (e.code === 'ArrowUp') {
+				this.selected = (this.selected - 1) % this.items.length
+				if (this.selected < 0)
+					this.selected = this.items.length - 1
+			}
+		})
+	}
+
+	hide = () => {
+		this.div.style.display = 'none'
+	}
+}
